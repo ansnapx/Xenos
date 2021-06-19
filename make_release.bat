63a5ceef5ea74e702a59811b55d4eb3f5db9b0b5
john.song <john.song@ucloud.cn>
2021-06-19 14:02:53 +0800

    add mkdir

--- make_release.bat
+++ make_release.bat
@@ -9,12 +9,13 @@ set ver=%1
 set zipper="C:\Program Files\7-Zip\7z.exe"
 
 mkdir %ver%
+mkdir Releases
 
 cd %ver%
 copy ..\build\Win32\Release\Xenos.exe Xenos.exe
 copy ..\build\x64\Release\Xenos64.exe Xenos64.exe
-copy ..\Releases\Changelog.txt Changelog.txt
-copy ..\Releases\Readme.txt Readme.txt
+::copy ..\Releases\Changelog.txt Changelog.txt
+::copy ..\Releases\Readme.txt Readme.txt
 %zipper% a -t7z Xenos_%ver%.7z * -mx=9 -myx=9
 copy Xenos_%ver%.7z ..\Releases\Xenos_%ver%.7z
 cd ..
